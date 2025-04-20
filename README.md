1079b383b3a521b9aec42b96d251bdab38dc5ff3
Amisha PRIYA <priyaamisha03@gmail.com>
2025-04-20 14:22:03 +0530

    Add styles + update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # QR-code-generator
 QR code generator using JS -> It uses the qrcode library, which is lightweight and easy to use.
+
+![Landing page](image.png)
+
+![Input focused](image-1.png)
+
+![QR code generated](image-2.png)
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-function generateQRCode() {
-    const qrCodeContainer = document.getElementById('qrCode');
+function generateQRCode(event) {
+    event.preventDefault();
+    const qrCodeContainer = document.getElementById('qr-code');
     const userInput = document.getElementById('userInput').value;
 
     qrCodeContainer.innerHTML = ''; // clear previous QR code
 
     if (userInput.trim()) {
       QRCode.toCanvas(document.createElement('canvas'), userInput, function (error, canvas) {
-        if (error) console.error(error);
-        else qrCodeContainer.appendChild(canvas);
-      });
+            if (error) console.error(error);
+            else {
+                canvas.style = "height: 15rem; width: 15rem";
+                qrCodeContainer.appendChild(canvas);
+            }
+        });
     } else {
       alert('Please enter text to generate QR code');
     }
